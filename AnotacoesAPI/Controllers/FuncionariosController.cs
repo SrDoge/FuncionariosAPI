cd44a8d1eb7783fe6e4daf01822cb3f9104a0406
julio.dias <julio.dias@DT-ECO-24>
2020-11-13 15:00:24 -0300

    Add project files.

---
+++ AnotacoesAPI/Controllers/FuncionariosController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AnotacoesAPI.Models;
+using MongoDB.Driver;
+
+
+namespace AnotacoesAPI.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        MongoDbContext dbContext = new MongoDbContext();
+        public IActionResult Index()
+        {
+            List<Funcionario> listaFuncs = dbContext.Funcionarios.Find(m => true).ToList();
+            return View(listaFuncs);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var entity = dbContext.Funcionarios.Find(m => m.Id == id).FirstOrDefault();
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Funcionario entity)
+        {
+            dbContext.Funcionarios.ReplaceOne(m => m.Id == entity.Id, entity);
+
+            return View(entity);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Funcionario entity)
+        {
+            entity.Id = Guid.NewGuid();
+
+            dbContext.Funcionarios.InsertOne(entity);
+
+            return RedirectToAction("Index", "Funcionarios");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            dbContext.Funcionarios.DeleteOne(m => m.Id == id);
+            return RedirectToAction("Index", "Funcionarios");
+        }
+    }
+}
---
+++ AnotacoesAPI/Models/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnotacoesAPI.Models
+{
+    public class Funcionario
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cidade { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Required]
+        public double Salario { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+    }
+}
